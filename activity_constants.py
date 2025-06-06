888a5c3d45ebf89380b188bd79d54afd2e2e2ae4
martinezg1976 <german.martinez@medinaandalucia.es>
2025-06-06 12:37:56 +0200

    Mail upload for first version

---
+++ activity_constants.py
@@ -0,0 +1,30 @@
+# Activity values identificator
+max_inactive_value = 2
+max_quest_value = 4
+max_active_value = 7
+
+# Folder names where data set are stored
+sub_folder_train = 'training'
+sub_folder_val = 'validation'
+sub_folder_test = 'test'
+sub_folder_act = 'active'
+sub_folder_inact = 'inactive'
+
+# Dimentions of raw data un columns and rows
+dataRegionEspMaxNumColumns = 1351
+dataRegionEspMaxNumRows = 300
+
+#File prefixers
+#prefix_activity_file = "ActivityData_"
+prefix_dataset_folder = "dataset_"
+prefix_model_file = "model_"
+
+
+#activity_file_folder = "/mnt/wd4tb/MEDINA/dl_activity/Publication1/MZActivity"
+#raw_data_folder ="/mnt/wd4tb/MEDINA/dl_activity/MZExtraction"
+
+# Data folder where training Dataset is stored
+data_set_folder = "ModelDataset"
+
+# Folder where differnets custom models are stored
+model_folder = "Model"
